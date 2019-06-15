5301579a85f5889f170cc3ae2e625a5bc3ba6cf3
James Little <littleguy23@gmail.com>
2019-06-15 12:47:44 -0400

    Add form to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,31 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import "../components/boilerform.css"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>404: Page not found.</h1>
+    <p>Sorry! We can't find the web page you're looking for. This is a mistake on our end.</p>
+    <p>We've logged this error and will investigate, but maybe you want to report what you were looking for?</p>
+    <form name="contact" action="/" method="POST" data-netlify="true" className="boilerform">
+      <div className="c-form__row">
+          <label for="name" className="c-label">Name (optional)</label>
+          <input type="text" name="name" id="name" className="c-input-field" />
+      </div>
+      <div className="c-form__row">
+          <label for="email" className="c-label">Email (optional)</label>
+          <input type="email" name="email" id="email" className="c-input-field" />
+      </div>
+      <div className="c-form__row">
+          <label for="message" className="c-label">What were you trying to find?</label>
+          <textarea name="message" id="message" className="c-input-field c-input-field--multiline" rows="10"></textarea>
+      </div>
+      <div className="c-form__row">
+          <button className="c-button" type="submit">Submit and go home</button>    
+      </div>
+    </form>
   </Layout>
 )
 
