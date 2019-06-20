373b3c9cf669dc2efa22c94903d0c39a55086a51
James Little <jameslittle230@users.noreply.github.com>
2019-06-20 17:46:33 -0400

    Add Dark Mode and Toggle
    Followed instructions here:
    https://www.gatsbyjs.org/blog/2019-01-31-using-react-context-api-with-g
    atsby/

---
+++ gatsby-browser.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+import { ThemeProvider } from "./src/context/themecontext"
+
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider>{element}</ThemeProvider>
+)
--- src/components/header.js
+++ src/components/header.js
@@ -2,17 +2,25 @@ import React from "react"
 import styles from "./header.module.css"
 import layoutStyles from "./layout.module.css"
 import Nav from "./navigation/nav"
+import ThemeContext from "../context/themecontext"
 
 const Header = () => (
-  <React.Fragment>
-    <div className={styles.headerBackground + ' ' + layoutStyles.headerBackground} />
-    <header className={styles.header + ' ' + layoutStyles.header}>
-      <h1>Drs. Roth, Rotter&nbsp;&amp;&nbsp;Laster</h1>
-      <p>637 Washington Street • Suite 202 • Brookline&nbsp;MA&nbsp;02446</p>
-      <p>(617) 232-2811</p>
-    </header>
-    <Nav />
-  </React.Fragment>
+  <ThemeContext.Consumer>
+    {theme => (
+      <React.Fragment>
+        <div className={styles.headerBackground + ' ' + layoutStyles.headerBackground} />
+        
+        <header className={styles.header + ' ' + layoutStyles.header}>
+          <button className="dark-switcher" onClick={theme.toggleDark}>{theme.dark ? <span>Light Mode</span> : <span>Dark Mode</span>}</button>
+          <h1>Drs. Roth, Rotter&nbsp;&amp;&nbsp;Laster</h1>
+          <p>637 Washington Street • Suite 202 • Brookline&nbsp;MA&nbsp;02446</p>
+          <p>(617) 232-2811</p>
+        </header>
+        
+        <Nav />
+      </React.Fragment>
+    )}
+  </ThemeContext.Consumer>
 )
 
 export default Header
