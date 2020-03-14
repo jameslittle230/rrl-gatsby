24e4ef40578f819883ca85f9a0485d2ab3143a46
James Little <littleguy23@gmail.com>
2020-03-13 17:42:22 -0700

    Covid content tweak

--- src/pages/covid19.js
+++ src/pages/covid19.js
@@ -7,24 +7,21 @@ const Covid19 = () => (
   <Layout>
     <SEO title="Covid-19" />
     <h1>COVID-19 Information</h1>
+    <p>For the safety of our patients we are changing our office procedures for the week of March 16.</p> 
     <ol>
-        <li>
-    For the safety of our patients we are changing our office procedures for the
-    next week.</li> 
-    <li>WELL VISITS: All well visits will be postponed and we will be
+    <li><strong>WELL VISITS:</strong> All well visits will be postponed and we will be
     in contact with you to reschedule. </li>
-    <li>SICK VISITS: If your child is sick
+    <li><strong>SICK VISITS:</strong> If your child is sick
     this week, please call.</li>
-    <li>MEDICATION FOLLOW UP APPOINTMENTS CURRENTLY
-    BOOKED: our office will contact you to manage the care. </li>
+    <li><strong>MEDICATION FOLLOW UP APPOINTMENTS CURRENTLY
+    BOOKED:</strong> our office will contact you to manage the care. </li>
     <li>Our patient
-    portal, MyChart, is the best way for care and communication. </li>
+    portal, <a href="https://mychart.chppoc.org/rrl/">MyChart</a>, is the best way for care and communication. </li>
     <li>If you have
     not signed up for MyChart (our patient portal) please call our office at
-    617- 232- 2811 and our staff is happy to sign you up. </li>
+    617-232-2811 and our staff is happy to sign you up. </li>
     <li>Remember we have
-    useful information for fever, vomiting, and diarrhea, and medication dosing
-    on our website: Rothrotterlaster.com </li>
+    useful information for <a href="https://rothrotterlaster.com/resources/">fever, vomiting, and diarrhea, and medication dosing.</a></li>
     <li>If you receive a telephone reminder
     regarding a visit this week, we are working with our provider, Televox, to
     transmit the following message:
