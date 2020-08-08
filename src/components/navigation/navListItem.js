c66e308fa62458b28cd2d2437313d76235f30d38
James Little <littleguy23@gmail.com>
2020-08-08 13:01:22 -0700

    Add things to nav

--- src/components/navigation/navListItem.js
+++ src/components/navigation/navListItem.js
@@ -1,16 +1,21 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Location } from '@reach/router';
+import { Location } from "@reach/router"
 import styles from "./nav.module.css"
 
-const NavListItem = (props) => (
-  <Location>
-    {({ location })=> (
-      <li className={location.pathname === props.to ? styles.active : ``}>
-            <Link to={props.to}>{props.children}</Link>
-      </li>
-    )}
-  </Location>
-)
+const NavListItem = ({ext, to, children}) =>
+  ext ? (
+    <li>
+      <a href={to}>{children} →</a>
+    </li>
+  ) : (
+    <Location>
+      {({ location }) => (
+        <li className={location.pathname === to ? styles.active : ``}>
+          <Link to={to}>{children}</Link>
+        </li>
+      )}
+    </Location>
+  )
 
 export default NavListItem
