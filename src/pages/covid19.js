6e3fae6d62bb7364360d46e2baef7f27c778bd5b
James Little <littleguy23@gmail.com>
2020-11-12 18:44:18 -0800

    Covid 19 content changes

--- src/pages/covid19.js
+++ src/pages/covid19.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 const Covid19 = () => (
   <Layout>
     <SEO title="Covid-19" />
-    <h1>COVID-19 Information</h1>
+    <h1>COVID-19 FAQs</h1>
 
     <p>
       Please help stop the spread of COVID-19 and wear your mask to cover your
@@ -16,63 +16,108 @@ const Covid19 = () => (
       apart, wearing masks.
     </p>
 
-    <h2>Testing Information</h2>
-
-    <h3>People without symptoms</h3>
+    <h3>
+      ILLNESS: If my child is sick, when should I get my child tested for
+      COVID-19?
+    </h3>
     <p>
-      If you do not have symptoms but would like a COVID-19 test because you are
-      returning from out of state, traveling out of state, or you are concerned
-      you were exposed, please refer to the following link to find a testing
-      site near you:{" "}
-      <a href="https://memamaps.maps.arcgis.com">
-        https://memamaps.maps.arcgis.com
-      </a>
+      Your child should get tested for COVID-19 if they are experiencing any of
+      the following symptoms:
+    </p>
+    <ul>
+      <li>Fever (100.0 or higher), chills, or shaking chills</li>
+      <li>Cough (not due to other known cause, such as chronic cough)</li>
+      <li>Difficulty breathing or shortness of breath</li>
+      <li>New loss of taste of smell</li>
+      <li>Sore throat</li>
+      <li>Headache when in combination with other symptoms</li>
+      <li>Muscle aches or body aches</li>
+      <li>Nausea, vomiting, or diarrhea</li>
+      <li>Fatigue, when in combination with other symptoms</li>
+      <li>
+        Nasal congestion or runny nose (not due to other known causes, such as
+        allergies) when in combination with other symptoms
+      </li>
+    </ul>
+
+    <h3>
+      EXPOSURE: My child was exposed to someone who tested positive and feels
+      fine
+    </h3>
+    <ul>
+      <p>
+        If your child was in contact for 15 minutes or more in a 24 hour period
+        less than six feet from someone then your child will need to quarantine
+        for 14 days and get a test.
+      </p>
+      <li>Do not do the test until 4 days have passed from the exposure.</li>
+      <li>
+        Your child will have to quarantine for 14 days from the last exposure
+        even if the COVID-19 test is negative.
+      </li>
+    </ul>
+
+    <h3>TESTING: How do I get my child tested?</h3>
+    <p>
+      If your child is sick, call the office and we will arrange a drive through
+      test at Boston Children’s Hospital. If your child is not sick or has been
+      exposed, you can find a testing site at the following 2 websites:
     </p>
 
-    <h3>People with symptoms</h3>
-    <p>Please call our office to arrange for testing.</p>
+    <ul>
+      <li>
+        <a href="http://memamaps.maps.arcgis.com/">memamaps.maps.arcgis.com</a>.
+        Enter your zip code and filter for "Referral not required". Then click
+        the search spyglass and check each site listed. The instructions for
+        each site will be there. If you are sick, insurance should pay. If you
+        are not sick, there is usually a charge.
+      </li>
+      <li>
+        Another website that shows testing sites that are free for all patients
+        is{" "}
+        <a href="https://www.mass.gov/info-details/stop-the-spread">
+          Stop the Spread
+        </a>
+        .
+      </li>
+      <li>
+        In addition, Brookline residents, students and teachers can get tested
+        at the Brookline Department of Public Health:
+        <a href="https://www.mpathy.org/schedule">
+          https://www.mpathy.org/schedule
+        </a>
+      </li>
+    </ul>
 
-    <h2>Office Visits</h2>
+    <h3>TRAVEL: What are the rules for travel?</h3>
     <p>
-      We are following a hybrid model with some in person visits and mostly
-      virtual visits. We will resume all in-person visits when we can according
-      to state and public health information.
+      If you return from travel for more than 24 hours to a place listed by the
+      state as high-risk, before you can leave your home you must:
     </p>
+    <li>Quarantine for 14 days from your arrival back in Massachusetts. </li>
+    <li>
+      OR have a negative test taken no longer than 72 hours before arrival in
+      Massachusetts
+    </li>
+    <li>OR have a negative test after arrival in Massachusetts </li>
+    <li>
+      Children age 10 years and younger are not required to take a test if
+      traveling with an adult from their household.
+    </li>
 
-    <ol>
-      <li>
-        <strong>Well Visits:</strong> All well visits should be scheduled at
-        their usual time. For our youngest patients who still require their baby
-        shots, we will conduct the visits with a virtual part and an in person
-        part. For older patients we will conduct a virtual visit and recommend a
-        follow up in-person visit if needed.
-      </li>
-      <li>
-        <strong>Sick visits:</strong> If your child is sick please call our
-        office. We are conducting all sick visits virtually and scheduling in
-        person if the illness warrants it.
-      </li>
-      <li>
-        Our patient portal,{" "}
-        <a href="https://mychart.chppoc.org/rrl/">MyChart</a>, is the best way
-        for care and communication. If you have not signed up for MyChart please
-        call our office at <a href="tel:6172322811">617-232-2811</a> and our
-        staff is happy to sign you up.
-      </li>
-      <li>
-        Remember we have useful information on our website for{" "}
-        <Link to="https://rothrotterlaster.com/resources/">
-          fever, vomiting, and diarrhea, and medication dosing.
-        </Link>
-      </li>
-    </ol>
-
-    <h2>Quarantine Information</h2>
+    <h3>What does quarantine mean?</h3>
     <p>
+      The person stays separate and masked even at home for 14 days.{" "}
       <a href="https://files.rothrotterlaster.com/covid-19-quarantine.pdf">
-        Click here to view/download a quarantine guide from the Mass. Department
-        of Public Health →
+        View/download a quarantine guide from the Mass. Department of Public
+        Health →
       </a>
+    </p>
+
+    <h3>What does isolation mean?</h3>
+    <p>
+      The sick person stays completely away in a separate room from anyone who
+      is not sick. All family members are masked.
     </p>
   </Layout>
 )
